65be58da391a860fb6bd7e7b93e66b0296be4c24
Drew Thompson <drewallynthompson@gmail.com>
2021-01-14 08:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function map (obj, callback){
 
 function reduce (obj, callback, accum){
   accum? accum : accum = 0;
-  console.log(accum);
+  console.log(!!accum);
   for (let i = 0; i < obj.length; i++){
     accum = callback(accum, obj[i]);
   }
